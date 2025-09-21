c727842491e69b634c744fc7481eaefa58b94b54
Divy547 <divyakaskgupta@gmail.com>
2025-09-21 14:29:52 +0530

    changed fetch call

--- app/page.js
+++ app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
     setResources([]);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://careeradviserbackend-production.up.railway.app/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ user_input: InputValue }),
